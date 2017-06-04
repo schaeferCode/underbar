0fb5e611a8f7959d5d0da4d64695e9e60bf44670
Scott Schaefer <scottschaef@gmail.com>
2017-06-04 16:05:06 -0400

    Return the last elements of an array

--- src/underbar.js
+++ src/underbar.js
@@ -38,7 +38,10 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    return n === undefined ? array[array.length-1] : array.slice(-n);
+    if (n > array.length) {
+        return array;
+    } else
+        return n === undefined ? array[array.length - 1] : array.slice(array.length - n, array.length);  
   };
 
   // Call iterator(value, key, collection) for each element of collection.
